1e70e527cd8fdf7adf613c2b8f5ec42d12a5807b
tnikhil02 <nikhilteki02@gmail.com>
2025-08-05 21:55:03 +0530

    "Day 1: Just Started ,added a storing students feature only backend"

---
+++ src/main/java/in/nik/main/controllers/listController.java
@@ -0,0 +1,34 @@
+package in.nik.main.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import in.nik.main.entity.Student;
+import in.nik.main.service.StudentService;
+
+@Controller
+public class listController {
+	StudentService serv;
+	@GetMapping("/update_std")
+	public String updatepage(Model model)
+	{
+		model.addAttribute("student",new Student());
+		return "update.html";
+	}
+	@PostMapping("/update_page")
+	public String addstdPage(@ModelAttribute("student") Student std,Model model) {
+		if(serv.updateStdbyid(std)) {
+			model.addAttribute("successMsg","Successfully updated");
+		}
+		else {
+			model.addAttribute("error","details were not updated due to mismatching id");
+		}
+		return "update.html";
+		
+	}
+
+}
